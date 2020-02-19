f61453e8ac2707d9db5202705cefac2503762c47
NeoxAzrot1 <sami.lepays@gmail.com>
2020-02-19 15:35:03 +0100

    Tris des cartes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,19 @@
 $('input[type=radio][name=trier]').change(function() {
+    var container = $('#sheet_container');
     var sheets = $('.sheet');
     var sheets_array = Array.prototype.slice.call(sheets, 0);
     var all_sheets_sort;
 
     if (this.value == 'difficulte') {
-        all_sheets_sort = sheets_array.map(c=>c).sort((a,b) => b.dataset.difficulty - a.dataset.difficulty)
+        all_sheets_sort = sheets_array.map(c=>c).sort((a,b) => a.dataset.difficulty - b.dataset.difficulty)
     }
     else if (this.value == 'temps') {
-        all_sheets_sort = sheets_array.map(c=>c).sort((a,b) => b.dataset.time - a.dataset.time)
+        all_sheets_sort = sheets_array.map(c=>c).sort((a,b) => a.dataset.time - b.dataset.time)
     }
+    
+    container.empty();
 
-    console.log(all_sheets_sort[0]);
+    for(var i = 0; i < all_sheets_sort.length; i++) {
+        container.append(all_sheets_sort[i]);
+    }
 });
