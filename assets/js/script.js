30b32e69e78421a54ec2b1559e1f426ae2d83c6e
NeoxAzrot1 <sami.lepays@gmail.com>
2020-02-19 17:04:41 +0100

    Button pour trier

--- assets/js/script.js
+++ assets/js/script.js
@@ -12,22 +12,25 @@ $(document).ready(function() {
 
 // Trie des fiches
 
-$('input[type=radio][name=trier]').change(function() {
-    var container = $('#sheet_container');
-    var sheets = $('.sheet');
-    var sheets_array = Array.prototype.slice.call(sheets, 0);
-    var all_sheets_sort;
+var container = $('#sheet_container');
+var sheets = $('.sheet');
+var sheets_array = Array.prototype.slice.call(sheets, 0);
+var all_sheets_sort;
 
-    if (this.value == 'difficulte') {
-        all_sheets_sort = sheets_array.map(c=>c).sort((a,b) => a.dataset.difficulty - b.dataset.difficulty)
-    }
-    else if (this.value == 'temps') {
-        all_sheets_sort = sheets_array.map(c=>c).sort((a,b) => a.dataset.time - b.dataset.time)
-    }
-    
+$('#difficulte').click(function() {
+    all_sheets_sort = sheets_array.map(c=>c).sort((a,b) => a.dataset.difficulty - b.dataset.difficulty);
+    showSheet();
+});
+
+$('#temps').click(function() {
+    all_sheets_sort = sheets_array.map(c=>c).sort((a,b) => a.dataset.time - b.dataset.time);
+    showSheet();
+});
+
+function showSheet() {
     container.empty();
-
+    
     for(var i = 0; i < all_sheets_sort.length; i++) {
         container.append(all_sheets_sort[i]);
     }
-});
+}
